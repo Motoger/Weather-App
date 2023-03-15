1849b74b0f4b10b6b82c72734548dd2a8b9309dd
Ilya Parshonak <parshonak@outlook.com>
2023-03-15 13:56:26 +0300

    Урок №: Работа с формами ч. 2 Продолжим работу с формами, а также закончим проект.

--- WetherApp/weather/urls.py
+++ WetherApp/weather/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('new_index/', views.new_index, name='new_index')
 ]
--- WetherApp/weather/views.py
+++ WetherApp/weather/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
 
 def index(request):
-    return render(request,'weather/index.html')
+    appid= '3722f0cf79a0574960364268f927a658'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
-def about(request):
-    pass
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+
+        all_cities.append(city_info)
+    context = {'all_info': all_cities,
+               'form': form}
+    return render(request,'weather/index.html',context)
+
+def new_index(request):
+    appid = '3722f0cf79a0574960364268f927a658'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = 'Minsk'
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"],
+        'visibility': res["visibility"],
+        'humidity': res["main"]["humidity"],
+        'pressure': res["main"]["pressure"],
+        'name': res["name"],
+        'weather': res["weather"][0]["main"],
+        'feel': res["main"]["feels_like"],
+        'wind': res["wind"]["speed"]
+    }
+    context = {'info': city_info}
+    return render(request,'weather/new_index.html', context)
